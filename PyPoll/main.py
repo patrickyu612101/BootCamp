571b66146343e171b8d0f7c2913cba3a4b5dd31c
Unknown <patrickyu612101@gmail.com>
2018-07-12 15:15:26 -0500

    Final Done! ......... maybe?

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 import pandas as pd
 
+text_file = open("Output.txt", "w")
 
 election_data = os.path.join("Resources", "election_data.csv")
 #pdFile= pd.read.csv(election_data,"encoding="ISO-8859-1")
@@ -10,16 +11,19 @@ total=pdFile["Voter ID"].count()    # get total votes
 
 listCandidate= pdFile["Candidate"].unique()   #get candidates with votes
 len=listCandidate.size  #get size of candidates
-print(len)
 listvotes=pdFile["Candidate"].value_counts() # also use this to calculate
 winner=""
 winnervote=0
 
 
 print("Election Results")
+text_file.write("Election Results\n")
 print("------------")
+text_file.write("------------\n")
 print("Total votes: "+str(total))
+text_file.write("Total votes: "+str(total)+"\n")
 print("------------")
+text_file.write("------------\n")
 i = 0
 for i in range(len) :   #test range is only in how much candidate, 
     percent= listvotes[i]/total *100    #find the percent
@@ -29,7 +33,14 @@ for i in range(len) :   #test range is only in how much candidate,
         winner = listCandidate[i]
 
     print(listCandidate[i]+" : "+str(listvotes[i])+" votes  ("+str(percent)+"%)")   #print the result by use the same index so I can add string between
+    text_file.write(listCandidate[i]+" : "+str(listvotes[i])+" votes  ("+str(percent)+"%)\n")
 
 print("------------")
+text_file.write("------------\n")
 print("Winner is : "+ winner)
+text_file.write("Winner is : "+ winner+"\n")
 print("------------")
+text_file.write("------------\n")
+
+
+text_file.close()
