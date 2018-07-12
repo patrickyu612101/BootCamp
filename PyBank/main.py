fdf3b21bfd04e3d0fc8358d124dd92eacbe89ff1
Unknown <patrickyu612101@gmail.com>
2018-07-12 11:12:52 -0500

    finish first one

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,20 +2,41 @@ import os
 import csv
 
 cereal_csv = os.path.join("Resources", "budget_data.csv")
-
+total_month=0
+total_Revenue=0
+maxN=0
+minN=0
+averageAll=0
+averageCount=0
+previous=0
 # Open and read csv
 with open(cereal_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     # Read the header row first (skip this part if there is no header)
-    #csv_header = next(csvfile)
-    #print(f"Header: {csv_header}")
+    csv_header = next(csvfile)
+    print(f"Header: {csv_header}")
 
     # Read through each row of data after the header
     for row in csvreader:
-        print(row)
-    
+        total_month=total_month+1   #calculate the total month
+        total_Revenue=total_Revenue+int(row[1]) 
+        averageCount=averageCount+1     # get the total row without header
+        if previous != 0:   #check if it is at start row
+            averageAll=averageAll+(int(row[1])-previous)
+        
+        if (int(row[1])-previous)>maxN: # get max and min number from change
+            maxN=int(row[1])-previous
+        elif (int(row[1])-previous)<minN:
+            minN = int(row[1])-previous
+        
+        previous = int(row[1])  #save the value for next run of loop to get the previous value
 
-        # Convert row to float and compare to grams of fiber
-        #if float(row[7]) >= 5:
-         #   print(row)
+
+print("The total moth is : "+str(total_month))  
+print("The total revenue is : "+str(total_Revenue))
+#print(str(averageAll))
+print("The Average Change is : "+str(averageAll/averageCount))
+print("The max change is: "+str(maxN))
+print("The min change is : "+str(minN))
+
